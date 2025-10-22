fff74347b553cb63b8342f90338c7f1e2fa3399e
kertoflenida <sskaverinass@gmail.com>
2025-10-22 16:08:51 +0300

    initial for lab

--- cmd/Wi-Fi-router-bandwidth-backend/main.go
+++ cmd/Wi-Fi-router-bandwidth-backend/main.go
@@ -3,9 +3,25 @@ package main
 import (
 	"Lab1/internal/api"
 	"log"
+    _ "Lab1/docs" 
 
 )
 
+
+// @title           API для лабораторной работы
+// @version         1.0
+// @description     Это сервер API с аутентификацией по JWT и управлением пользователями.
+
+// @contact.name   Kaverina 
+// @contact.url    https://t.me/ya_kaverina
+
+// @host      localhost:8080
+// @BasePath  /api
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description "Для доступа к защищенным методам введите 'Bearer ' и после пробела ваш JWT токен."
 func main(){
 	log.Println("Application start!")
 	api.StartServer()
